f182be90d5c88c1b192485cb716135066c809f38
rasimgt <rasimgt88@gmail.com>
2024-09-20 22:42:10 +0300

    Исправление Рамзиль

--- z3.py
+++ z3.py
@@ -1,11 +1,12 @@
-import telebot;
-bot = telebot.TeleBot('7239840132:AAES-Q82uIqGgSxK5ekglgI-iDnZAi53eK0'); #тут токен бота
-@bot.message_handler(content_types=['text']) #слушаем бота
+import telebot
+bot = telebot.TeleBot('7239840132:AAES-Q82uIqGgSxK5ekglgI-iDnZAi53eK0')  # здесь ваш токен
+@bot.message_handler(content_types=['text'])  # слушаем текстовые сообщения
 def get_text(message):
-    if message.text == "Привет": #проверям сообщение от пользователя
-        bot.send_message(message.from_user.id, "Здравствуй, мой дорогой друг!") #отвечаем пользователю
+    if message.text == "Привет":  # проверяем сообщение от пользователя
+        bot.send_message(message.chat.id, "Здравствуй, мой дорогой друг!")  # отвечаем пользователю
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "напиши: Привет")
+        bot.send_message(message.chat.id, "напиши: Привет")
     else:
-        bot.send_message(message.from_user.id, "я тебя не понимаю, напиши '/help'")
-bot.polling(none_stop=True, interval=0)# бот постоянно будет опрашивает сервер
+        bot.send_message(message.chat.id, "я тебя не понимаю, напиши '/help'")
+
+bot.polling(none_stop=True, interval=0)  # бот постоянно будет опрашивать сервер
