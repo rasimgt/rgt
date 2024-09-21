353102b18e1a6371b4b45466a3e3e87fb035bf9d
rasimgt <rasimgt88@gmail.com>
2024-09-21 11:35:29 +0300

    тестовый

--- z3.py
+++ z3.py
@@ -2,7 +2,7 @@ import telebot
 bot = telebot.TeleBot('7239840132:AAES-Q82uIqGgSxK5ekglgI-iDnZAi53eK0')  # здесь ваш токен
 @bot.message_handler(content_types=['text'])  # слушаем текстовые сообщения
 def get_text(message):
-    if message.text == "Привет":  # проверяем сообщение от пользователя
+    if message.text == "Привет!":  # проверяем сообщение от пользователя
         bot.send_message(message.chat.id, "Здравствуй, мой дорогой друг!")  # отвечаем пользователю
     elif message.text == "/help":
         bot.send_message(message.chat.id, "напиши: Привет")
