2aaa22b0f66591b249b1d0b362325f8b60631df4
rasimgt <rasimgt88@gmail.com>
2024-10-08 19:12:38 +0300

    bot 08 2

--- bot08.py
+++ bot08.py
@@ -5,3 +5,25 @@ import os
 load_dotenv()
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
+
+@bot.message_handler(commands=['start'])
+def send_start(message):
+#    bot.send_message(message.from_user.id, 'Привет!')   #декоратор. после написания старт выходит сообщение привет
+    if message.text == '\start':
+        bot.send_message(message.from_user.id, 'Hello!')
+    else:
+        if message.text == '\help':
+            bot.send_message(message.from_user.id, 'Это помощь')
+
+
+@bot.message_handler(content_types=['text'])
+
+#def get_message(message):
+#    bot.send_message(message.from_user.id, message.text)  #просто ответ
+
+def reply_text(message):
+    bot.reply_to(message, message.text)    # бот отправляет ответное письмо
+
+
+
+bot.polling(non_stop=True)
